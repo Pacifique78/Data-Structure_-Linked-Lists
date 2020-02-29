fbd424f377e0a7322339571d4a50d2adc940917e
Pacifique78 <tuyipaccy78@gmail.com>
2020-02-29 21:00:04 +0200

    Add Insert at a given position on double linked list

--- doubleLinked list.c
+++ doubleLinked list.c
@@ -8,7 +8,7 @@ struct node{
 main(){
     int c;
     do{
-        printf("\n1.Push\n2.Append\n3.display forward\n4.display backward\n5.delete\n6.insert(after value)\n0.Exit\nMake a choice");
+        printf("\n1.Push\n2.Append\n3.display forward\n4.display backward\n5.delete\n6.insert(after value)\n7.insert(at a position)\n0.Exit\nMake a choice: ");
         scanf("%d", &c);
         switch(c){
         case 1:
@@ -29,6 +29,9 @@ main(){
         case 6:
             insert();
             break;
+        case 7:
+            insertAtPosition();
+            break;
         case 0:
             printf("bye");
             break;
@@ -48,6 +51,9 @@ display_forward(){
 }
 display_backward(){
     struct node *p;
+    if(h == NULL){
+        return;
+    }
     p = h;
     while(p->next)
         p=p->next;
@@ -89,11 +95,21 @@ append(){
 delete(){
     int num;
     struct node *p, *q;
+    printf("\nEnter the number to delete: ");
     scanf("%d", &num);
+    if(!h){
+        printf("\nThe list is empty: ");
+        return;
+    }
     if(h->data==num){
+        if(!h->next){
+            h=NULL;
+            return;
+        }
         h = h->next;
         h->previous = NULL;
         free(q);
+        return;
     }
     else{
         q = h;
@@ -104,16 +120,22 @@ delete(){
             }
             q = q->next;
         }
-        p = q->next;
-        q->previous->next = p;
-        p->previous = q->previous;
+        if(q->next == NULL){
+            q->previous->next = NULL;
+            q->previous = NULL;
+        }
+        else{
+            p = q->next;
+            q->previous->next = p;
+            p->previous = q->previous;
+        }
     }
 }
 insert(){
     struct node *p, *q, *z;
     p = (struct node*)malloc(sizeof(struct node));
     int num;
-    printf("enter the number to search");
+    printf("enter the number to search: ");
     scanf("%d", &num);
     q = h;
     while(q->data != num){
@@ -123,7 +145,7 @@ insert(){
         }
         q = q->next;
     }
-    printf("enter to number to add");
+    printf("enter to number to add: ");
     scanf("%d", &p->data);
     if(q->next == NULL){
         q->next=p;
@@ -137,4 +159,63 @@ insert(){
         p->previous=q;
         q->next=p;
     }
+}
+int counter(){
+    int count=0;
+    struct node *q;
+    if(h == NULL){
+        return 0;
+    }
+    q = h;
+    while(q){
+        count ++;
+        q = q->next;
+    }
+    return count;
+}
+insertAtPosition(){
+    struct node *p, *q;
+    p = (struct node*) malloc(sizeof(struct node));
+    int position;
+    printf("After which position do want to add: ");
+    scanf("%d", &position);
+    if(h==NULL){
+        printf("Number to add: ");
+        scanf("%d", &p->data);
+        p->next=p->previous = NULL;
+        h=p;
+    }
+    else{
+        if(position-1 > counter() || position < 1){
+            printf("The position you have chosen does exist");
+            return;
+        }
+        else{
+            q = h;
+            printf("\nEnter the number to add: ");
+            scanf("%d", &p->data);
+            if(position == 1){
+                p->next=q;
+                q->previous=p;
+                p->previous=NULL;
+                h=p;
+            }
+            else{
+                for(int i=1; i<position-1; i++){
+                    q = q->next;
+                }
+                if(!q->next){
+                    q->next = p;
+                    p->previous = q;
+                    p->next = NULL;
+                    return;
+                }
+                p->next = q->next;
+                q->next->previous = p;
+                q->next = p;
+                p->previous = q;
+            }
+        }
+    }
+
 }
